a78f0b38b94815f4cd86d459737529fc70ef36ec
Per Olav Flaten <perolavflaten@live.no>
2016-01-26 13:52:55 +0100

    Added Hello World code base

---
+++ src/MotionWordPlay/Code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NTNU.MotionWordPlay
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
